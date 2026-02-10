d08379fa2dfbf6c3442d1da3ff9d9ab1951db4bf
DustVoice <info@dustvoice.de>
2026-02-10 21:41:49 +0000

    Add git config

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -1,13 +1,65 @@
+let
+  dependencies.programs = {
+    difftastic = {
+      enable = true;
+      git = {
+        enable = true;
+        diffToolMode = true;
+      };
+    };
+    
+    helix.enable = true;
+  };
+in
 {
   dev.git.homeManager = {
+    inherit dependencies;
+    
     programs.git = {
       enable = true;
+
       settings = {
-        user.name = "DustVoice";
-        user.email  = "info@dustvoice.de";
+        user = {
+          name = "DustVoice";
+          email  = "info@dustvoice.de";
+        };
+
+        core = {
+          autocrlf = "input";
+          editor = "hx";
+          attributesfile = "~/.gitattributes";
+        };
+
+        push = {
+          default = "simple";
+          followTags = true;
+        };
+
+        pull.rebase = false;
+
+        http.postBuffer = "500M";
+
+        merge.conflictstyle = "diff3";
+
+        diff.colorMoved = "default";
 
         init.defaultBranch = "main";
       };
+
+      ignores = [
+        ".DS_Store"
+        "*.swp"
+        ".direnv"
+        ".envrc"
+        ".envrc.local"
+        ".env"
+        ".env.local"
+        ".jj"
+        "devshell.toml"
+        ".tool-versions"
+      ];
+
+      lfs.enable = true;
     };
   };
 }
