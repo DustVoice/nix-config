baf0d469e88d11201e9d4bb471203e933a232d1f
DustVoice <info@dustvoice.de>
2026-02-13 11:23:05 +0000

    Finally fix Proxy issues with env_keep. autoProxy of WSL already sets the proxy correctly

--- modules/my/machine.nix
+++ modules/my/machine.nix
@@ -13,20 +13,36 @@
       { pkgs, ... }:
       {
         environment.systemPackages = [ pkgs.git pkgs.vim ];
+
+        security.sudo-rs = {
+          enable = true;
+      	  wheelNeedsPassword = false;
+
+          extraConfig = ''
+            Defaults env_keep += "http_proxy"
+            Defaults env_keep += "https_proxy"
+            Defaults env_keep += "HTTP_PROXY"
+            Defaults env_keep += "HTTPS_PROXY"
+          '';
+        };
       };
 
-    wsl.nixos = {
-      imports = [
-        inputs.nixos-wsl.nixosModules.default
-      ];
+    wsl.nixos =
+      { pkgs, ... }:
+      {
+        imports = [
+          inputs.nixos-wsl.nixosModules.default
+        ];
 
-      wsl = {
-        enable = true;
-        startMenuLaunchers = true;
-        usbip.enable = true;
-        useWindowsDriver = true;
+        wsl = {
+          enable = true;
+          startMenuLaunchers = true;
+          usbip.enable = true;
+          useWindowsDriver = true;
+        };
+
+        environment.systemPackages = [ pkgs.wsl-vpnkit ];
       };
-    };
 
     wsl-home.includes = [
       <my.machine/base>
@@ -36,12 +52,6 @@
     wsl-work-old.includes = [
       <my.machine/base>
       <my.machine/wsl>
-      (<nix-config/proxy>
-        {
-          proxy = "http://ldiproxy.lsjv.rlp.de:8080";
-          noProxy = "vsvr-*;svr-*;nas-*;web-ko-*;lsjvko*;lsjvmz*;www.lsjv.rlp;www-sps.lsjv.rlp;klr.lsjv.rlp;mdtm.lsjv.rlp;bibliothek.lsjv.rlp;10.5.*;localhost;127.0.0.1;*-ldi.rlp.de;webmail.lsjv.rlp.de;webmail.lsjv.de;autodiscover.lsjv.rlp.de;tickets.lsjv.rlp.de;i-doit.lsjv.rlp.de;citrix.lsjv.rlp.de;director.lsjv.rlp.de;eudragmdp.eudra.org;ecdm.eudra.org;openproject.lsjv.rlp;login.lsjv.de";
-        }
-      )
     ];
 
     wsl-work = {
@@ -49,12 +59,6 @@
         <my.machine/base>
         <my.machine/wsl>
       ];
-
-      nixos =
-        { pkgs, ... }:
-        {
-          environment.systemPackages = [ pkgs.wsl-vpnkit ];
-        };
     };
   };
 }
