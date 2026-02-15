5593e70f4d5c0d86e54d3951bb4dcadc5aac9b38
DustVoice <info@dustvoice.de>
2026-02-15 22:33:35 +0000

    Add vm.nix

---
+++ modules/vm.nix
@@ -0,0 +1,13 @@
+{ __findFile, inputs, den, ... }: {
+  perSystem = { pkgs, ... }: {
+    packages.vm = pkgs.writeShellApplication {
+      name = "vm";
+      text =
+        let
+          host = inputs.self.nixosConfigurations.wsl-home.config;
+        in ''
+          ${host.system.build.vm}/bin/run-${host.networking.hostName}-vm "$@"
+        '';
+    };
+  };
+}
