aa4f8ecfc07fa1fc4519d123851fff81a2f8b678
DustVoice <info@dustvoice.de>
2026-02-22 16:54:53 +0000

    Update and try to resolve unfree error

--- modules/dev/ai.nix
+++ modules/dev/ai.nix
@@ -5,30 +5,35 @@
 }: let
   flake-file.inputs.claude-code.url = "github:sadjow/claude-code-nix";
 
-  claude-code = {
-    # includes = [
-    #   (<den/unfree> [
-    #     "claude-code"
-    #     "claude-code-node"
-    #     "claude-code-bun"
-    #   ])
-    # ];
+  claude-code = variant:
+    <den.lib.parametric> {
+      includes = [
+        ({user, ...}:
+          <den/unfree> [
+            variant
+          ])
+      ];
 
-    nixos.nix.settings = {
-      substituters = ["https://claude-code.cachix.org"];
-      trusted-public-keys = ["claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="];
+      nixos.nix.settings = {
+        substituters = ["https://claude-code.cachix.org"];
+        trusted-public-keys = ["claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="];
+      };
+
+      homeManager = {pkgs, ...}: {
+        nixpkgs.overlays = [inputs.claude-code.overlays.default];
+        home.packages = [pkgs.${variant}];
+        home.shellAliases = {
+          claude =
+            {
+              "claude-code" = "claude";
+              "claude-code-bun" = "claude-bun";
+              "claude-code-node" = "claude-node";
+            }.${
+              variant
+            };
+        };
+      };
     };
-
-    # nixos = {
-    #   imports = [
-    #     inputs.claude-code.overlays.default
-    #   ];
-    # };
-
-    homeManager = {pkgs, ...}: {
-      # home.packages = [pkgs.claude-code-bun];
-    };
-  };
 
   helix-assist.homeManager.programs.helix.languages = {
     language-server.helix-assist = {
@@ -39,8 +44,10 @@
 in {
   inherit flake-file;
 
-  dev.ai.includes = [
-    claude-code
-    helix-assist
-  ];
+  dev.ai = <den.lib.parametric> {
+    includes = [
+      (claude-code "claude-code-bun")
+      helix-assist
+    ];
+  };
 }
