9b3aa66f288e401bc8880a7514e0a842acf6958a
DustVoice <info@dustvoice.de>
2026-02-23 20:05:08 +0000

    Extend toml config

--- modules/dev/langs/toml.nix
+++ modules/dev/langs/toml.nix
@@ -1,5 +1,17 @@
-{
-  dev.langs.provides.toml.homeManager = {pkgs, ...}: {
+let
+  tombi.homeManager = {pkgs, ...}: {
     home.packages = [pkgs.tombi];
   };
+  helix-languages.homeManager.programs.helix.languages.language = [
+    {
+      name = "toml";
+      auto-format = true;
+      formatter.command = "tombi format";
+    }
+  ];
+in {
+  dev.langs.provides.toml.includes = [
+    tombi
+    helix-languages
+  ];
 }
