2004bd3d5af2449b77bdf1e690f6273e9bc8872f
DustVoice <info@dustvoice.de>
2026-04-10 12:19:25 +0000

    Add nh utilitiies for flake-less

---
+++ nh.nix
@@ -0,0 +1,16 @@
+# Provides shell utilities under `den.sh` for building OS configurations using
+# github:nix-community/nh instead of nixos-rebuild, etc
+{
+  lib,
+  den,
+  inputs,
+  ...
+}: {
+  options.den.sh = lib.mkOption {
+    description = "Non-flake Den shell environment";
+    default = den.lib.nh.denShell {
+      fromFlake = false;
+      outPrefix = ["flake"];
+    } (import inputs.nixpkgs {});
+  };
+}
